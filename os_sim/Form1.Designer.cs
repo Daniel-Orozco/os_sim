f48d332b6ff351784265fd2dcfa171a5b16ed377
Luis_Orozco <geoluisd@hotmail.com>
2015-09-29 09:10:39 -0700

    backup

--- os_sim/Form1.Designer.cs
+++ os_sim/Form1.Designer.cs
@@ -37,12 +37,21 @@
             this.new_list = new System.Windows.Forms.TextBox();
             this.ready_list = new System.Windows.Forms.TextBox();
             this.running_list = new System.Windows.Forms.TextBox();
+            this.clock_display = new System.Windows.Forms.TextBox();
+            this.quantum_display = new System.Windows.Forms.TextBox();
+            this.finished_list = new System.Windows.Forms.TextBox();
+            this.waiting_list = new System.Windows.Forms.TextBox();
+            this.waiting_queue = new System.Windows.Forms.Label();
+            this.io1_queue = new System.Windows.Forms.Label();
+            this.io1_list = new System.Windows.Forms.TextBox();
+            this.pcb_list = new System.Windows.Forms.TextBox();
+            this.pcb_id = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // clock
             // 
             this.clock.AutoSize = true;
-            this.clock.Location = new System.Drawing.Point(38, 36);
+            this.clock.Location = new System.Drawing.Point(38, 27);
             this.clock.Name = "clock";
             this.clock.Size = new System.Drawing.Size(34, 13);
             this.clock.TabIndex = 0;
@@ -51,7 +60,7 @@
             // quantum
             // 
             this.quantum.AutoSize = true;
-            this.quantum.Location = new System.Drawing.Point(116, 36);
+            this.quantum.Location = new System.Drawing.Point(116, 27);
             this.quantum.Name = "quantum";
             this.quantum.Size = new System.Drawing.Size(50, 13);
             this.quantum.TabIndex = 1;
@@ -61,7 +70,7 @@
             // 
             this.new_queue.AutoSize = true;
             this.new_queue.BackColor = System.Drawing.SystemColors.Control;
-            this.new_queue.Location = new System.Drawing.Point(38, 75);
+            this.new_queue.Location = new System.Drawing.Point(38, 84);
             this.new_queue.Name = "new_queue";
             this.new_queue.Size = new System.Drawing.Size(29, 13);
             this.new_queue.TabIndex = 2;
@@ -71,7 +80,7 @@
             // 
             this.ready_queue.AutoSize = true;
             this.ready_queue.BackColor = System.Drawing.SystemColors.Control;
-            this.ready_queue.Location = new System.Drawing.Point(89, 75);
+            this.ready_queue.Location = new System.Drawing.Point(150, 84);
             this.ready_queue.Name = "ready_queue";
             this.ready_queue.Size = new System.Drawing.Size(38, 13);
             this.ready_queue.TabIndex = 3;
@@ -81,7 +90,7 @@
             // 
             this.running_queue.AutoSize = true;
             this.running_queue.BackColor = System.Drawing.SystemColors.Control;
-            this.running_queue.Location = new System.Drawing.Point(146, 75);
+            this.running_queue.Location = new System.Drawing.Point(262, 84);
             this.running_queue.Name = "running_queue";
             this.running_queue.Size = new System.Drawing.Size(47, 13);
             this.running_queue.TabIndex = 4;
@@ -91,7 +100,7 @@
             // 
             this.finished_queue.AutoSize = true;
             this.finished_queue.BackColor = System.Drawing.SystemColors.Control;
-            this.finished_queue.Location = new System.Drawing.Point(209, 75);
+            this.finished_queue.Location = new System.Drawing.Point(486, 84);
             this.finished_queue.Name = "finished_queue";
             this.finished_queue.Size = new System.Drawing.Size(46, 13);
             this.finished_queue.TabIndex = 5;
@@ -124,15 +133,117 @@
             this.running_list.Multiline = true;
             this.running_list.Name = "running_list";
             this.running_list.ReadOnly = true;
-            this.running_list.Size = new System.Drawing.Size(106, 114);
+            this.running_list.Size = new System.Drawing.Size(106, 20);
             this.running_list.TabIndex = 8;
             this.running_list.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // clock_display
+            // 
+            this.clock_display.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.clock_display.Location = new System.Drawing.Point(41, 43);
+            this.clock_display.Multiline = true;
+            this.clock_display.Name = "clock_display";
+            this.clock_display.ReadOnly = true;
+            this.clock_display.Size = new System.Drawing.Size(60, 20);
+            this.clock_display.TabIndex = 9;
+            // 
+            // quantum_display
+            // 
+            this.quantum_display.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.quantum_display.Location = new System.Drawing.Point(119, 43);
+            this.quantum_display.Multiline = true;
+            this.quantum_display.Name = "quantum_display";
+            this.quantum_display.ReadOnly = true;
+            this.quantum_display.Size = new System.Drawing.Size(60, 20);
+            this.quantum_display.TabIndex = 10;
+            // 
+            // finished_list
+            // 
+            this.finished_list.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.finished_list.Location = new System.Drawing.Point(489, 100);
+            this.finished_list.Multiline = true;
+            this.finished_list.Name = "finished_list";
+            this.finished_list.ReadOnly = true;
+            this.finished_list.Size = new System.Drawing.Size(106, 114);
+            this.finished_list.TabIndex = 11;
+            // 
+            // waiting_list
+            // 
+            this.waiting_list.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.waiting_list.Location = new System.Drawing.Point(377, 100);
+            this.waiting_list.Multiline = true;
+            this.waiting_list.Name = "waiting_list";
+            this.waiting_list.ReadOnly = true;
+            this.waiting_list.Size = new System.Drawing.Size(106, 114);
+            this.waiting_list.TabIndex = 12;
+            this.waiting_list.TextChanged += new System.EventHandler(this.waiting_list_TextChanged);
+            // 
+            // waiting_queue
+            // 
+            this.waiting_queue.AutoSize = true;
+            this.waiting_queue.BackColor = System.Drawing.SystemColors.Control;
+            this.waiting_queue.Location = new System.Drawing.Point(374, 84);
+            this.waiting_queue.Name = "waiting_queue";
+            this.waiting_queue.Size = new System.Drawing.Size(43, 13);
+            this.waiting_queue.TabIndex = 13;
+            this.waiting_queue.Text = "Waiting";
+            this.waiting_queue.Click += new System.EventHandler(this.waiting_queue_Click);
+            // 
+            // io1_queue
+            // 
+            this.io1_queue.AutoSize = true;
+            this.io1_queue.BackColor = System.Drawing.SystemColors.Control;
+            this.io1_queue.Location = new System.Drawing.Point(265, 122);
+            this.io1_queue.Name = "io1_queue";
+            this.io1_queue.Size = new System.Drawing.Size(53, 13);
+            this.io1_queue.TabIndex = 14;
+            this.io1_queue.Text = "Using I/O";
+            // 
+            // io1_list
+            // 
+            this.io1_list.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.io1_list.Location = new System.Drawing.Point(265, 138);
+            this.io1_list.Multiline = true;
+            this.io1_list.Name = "io1_list";
+            this.io1_list.ReadOnly = true;
+            this.io1_list.Size = new System.Drawing.Size(106, 20);
+            this.io1_list.TabIndex = 15;
+            // 
+            // pcb_list
+            // 
+            this.pcb_list.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pcb_list.Location = new System.Drawing.Point(41, 248);
+            this.pcb_list.Multiline = true;
+            this.pcb_list.Name = "pcb_list";
+            this.pcb_list.ReadOnly = true;
+            this.pcb_list.Size = new System.Drawing.Size(554, 388);
+            this.pcb_list.TabIndex = 16;
+            // 
+            // pcb_id
+            // 
+            this.pcb_id.AutoSize = true;
+            this.pcb_id.BackColor = System.Drawing.SystemColors.Control;
+            this.pcb_id.Location = new System.Drawing.Point(43, 232);
+            this.pcb_id.Name = "pcb_id";
+            this.pcb_id.Size = new System.Drawing.Size(29, 13);
+            this.pcb_id.TabIndex = 17;
+            this.pcb_id.Text = "New";
+            this.pcb_id.Click += new System.EventHandler(this.label1_Click);
             // 
             // mainView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1272, 693);
+            this.Controls.Add(this.pcb_id);
+            this.Controls.Add(this.pcb_list);
+            this.Controls.Add(this.io1_list);
+            this.Controls.Add(this.io1_queue);
+            this.Controls.Add(this.waiting_queue);
+            this.Controls.Add(this.waiting_list);
+            this.Controls.Add(this.finished_list);
+            this.Controls.Add(this.quantum_display);
+            this.Controls.Add(this.clock_display);
             this.Controls.Add(this.running_list);
             this.Controls.Add(this.ready_list);
             this.Controls.Add(this.new_list);
@@ -160,6 +271,15 @@
         private System.Windows.Forms.TextBox new_list;
         private System.Windows.Forms.TextBox ready_list;
         private System.Windows.Forms.TextBox running_list;
+        private System.Windows.Forms.TextBox clock_display;
+        private System.Windows.Forms.TextBox quantum_display;
+        private System.Windows.Forms.TextBox finished_list;
+        private System.Windows.Forms.TextBox waiting_list;
+        private System.Windows.Forms.Label waiting_queue;
+        private System.Windows.Forms.Label io1_queue;
+        private System.Windows.Forms.TextBox io1_list;
+        private System.Windows.Forms.TextBox pcb_list;
+        private System.Windows.Forms.Label pcb_id;
     }
 }
 
